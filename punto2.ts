8f1bf834a5069572ffd1add1ce5ddedbefdf7c43
SebasMejia1 <sebastianmejiasanchez1030@gmail.com>
2024-09-24 21:52:28 -0500

    Ejercicio nro 2

---
+++ punto2.ts
@@ -0,0 +1,23 @@
+import { Persona } from "./punto1";
+
+const persona1 = new Persona("Benito", 20);
+const persona2 = new Persona("Antonio", 15);
+const persona3 = new Persona("Martinez", 2);
+const persona4 = new Persona("Ocasio", 5);
+const persona5 = new Persona("Directamente", 30);
+const persona6 = new Persona("Del espacio", 12);
+const persona7 = new Persona("ey", 12);
+const persona8 = new Persona("Me pongo", 19);
+const persona9 = new Persona("Rolex como", 18);
+const persona10 = new Persona("Si fueran casio", 35);
+
+persona1.saludar()
+persona2.saludar()
+persona3.saludar()
+persona4.saludar()
+persona5.saludar()
+persona6.saludar()
+persona7.saludar()
+persona8.saludar()
+persona9.saludar()
+persona10.saludar()
